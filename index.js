314c40afa48637d5800aacb8cfa006fb4041a8f4
MisterPowering <50859607+MisterPowering@users.noreply.github.com>
2024-11-21 14:47:50 +1300

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["CODE: THEHILLS", "Beverly Hills CHP"];
-const statusTypes = [ 'dnd', 'online'];
+const statusMessages = [".gg/bevhills"];
+const statusTypes = [ 'online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
